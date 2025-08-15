63ce583e6c8b9157f578d7970cd5001f3389742d
Vitalii <vitalyoirshko@gmail.com>
2025-08-15 18:19:59 +0300

    fixed

--- task.sql
+++ task.sql
@@ -1,18 +1,30 @@
 -- Use our database
-USE ShopDB; 
+USE ShopDB;
 
-START TRANSACTION; 
+START TRANSACTION;
 
-INSERT INTO Orders (CustomerID, Date)
-VALUES ('1', '2023-01-01');
+SELECT WarehouseAmount
+INTO @stock
+FROM Products
+WHERE ID = 1
+FOR UPDATE;  
 
-SET @new_order_id = LAST_INSERT_ID();
+IF @stock < 1 THEN
+    ROLLBACK;
+    SELECT 'Not enough stock. Transaction cancelled.' AS Message;
+ELSE
+    INSERT INTO Orders (CustomerID, Date)
+    VALUES ('1', '2023-01-01');
 
-INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (@new_order_id, 1, 1);
+    SET @new_order_id = LAST_INSERT_ID();
 
-UPDATE Products
-SET WarehouseAmount = WarehouseAmount - 1
-WHERE ID = 1;
+    INSERT INTO OrderItems (OrderID, ProductID, Count)
+    VALUES (@new_order_id, 1, 1);
 
-COMMIT; 
+    UPDATE Products
+    SET WarehouseAmount = WarehouseAmount - 1
+    WHERE ID = 1;
+
+    COMMIT;
+    SELECT 'Order successfully placed.' AS Message;
+END IF; 
